eaed278364e73ea81af8987a1b7a4869a074ef33
John Barton (joho) <jrbarton@gmail.com>
2018-08-26 16:24:01 +1000

    rejig background script to properly work on pageload

--- src/background.ts
+++ src/background.ts
@@ -1,55 +1,48 @@
 import { UIRatingMessage, BackgroundRatingMessage } from "./util/types";
-import { ObservableMap, observe } from "mobx";
+import { ObservableMap } from "mobx";
 import { setRating, logView } from "./util/api";
 
-chrome.runtime.onInstalled.addListener(() => {
-  const prs: ObservableMap<string, number> = new ObservableMap();
+const prs: ObservableMap<string, number> = new ObservableMap();
+const ports: chrome.runtime.Port[] = [];
 
-  let apiKey: string;
-  chrome.storage.sync.get("apiKey", items => {
-    apiKey = items.apiKey;
-  });
-  chrome.storage.onChanged.addListener((changes, areaName) => {
-    if (areaName === "sync") {
-      if (changes.apiKey) {
-        apiKey = changes.apiKey.newValue;
-      }
+let apiKey: string;
+chrome.storage.sync.get("apiKey", items => {
+  apiKey = items.apiKey;
+});
+
+chrome.storage.onChanged.addListener((changes, areaName) => {
+  if (areaName === "sync") {
+    if (changes.apiKey) {
+      apiKey = changes.apiKey.newValue;
+    }
+  }
+});
+
+chrome.runtime.onConnect.addListener(port => {
+  ports.push(port);
+  const prPath = port.name;
+  if (!prs.has(prPath)) {
+    prs.set(prPath, null);
+  }
+
+  const cancel = prs.observe(change => {
+    if (change.type === "update" && change.name === prPath) {
+      const msg: BackgroundRatingMessage = { rating: change.newValue };
+      port.postMessage(msg);
     }
   });
-
-  chrome.runtime.onConnect.addListener(port => {
-    const prPath = port.name;
-    if (!prs.has(prPath)) {
-      prs.set(prPath, null);
+  port.onDisconnect.addListener(() => {
+    const i = ports.indexOf(port);
+    if (i > -1) {
+      ports.splice(i, 1);
     }
+    cancel();
+  });
 
-    const cancel = prs.observe(change => {
-      if (change.type === "update" && change.name === prPath) {
-        const msg: BackgroundRatingMessage = { rating: change.newValue };
-        port.postMessage(msg);
-      }
-    });
-    port.onDisconnect.addListener(() => {
-      cancel();
-    });
-
-    port.onMessage.addListener((msg: UIRatingMessage) => {
-      setRating(apiKey, msg.prPath, msg.rating)
-        .then(resp => {
-          prs.set(msg.prPath, resp.rating);
-        })
-        .catch(reason => {
-          const msg: BackgroundRatingMessage = {
-            rating: prs.get(prPath),
-            error: reason
-          };
-          port.postMessage(msg);
-        });
-    });
-
-    logView(apiKey, prPath)
+  port.onMessage.addListener((msg: UIRatingMessage) => {
+    setRating(apiKey, msg.prPath, msg.rating)
       .then(resp => {
-        prs.set(prPath, resp.rating);
+        prs.set(msg.prPath, resp.rating);
       })
       .catch(reason => {
         const msg: BackgroundRatingMessage = {
@@ -59,4 +52,23 @@ chrome.runtime.onInstalled.addListener(() => {
         port.postMessage(msg);
       });
   });
+
+  logView(apiKey, prPath)
+    .then(resp => {
+      if (prs.get(prPath) === resp.rating) {
+        const msg: BackgroundRatingMessage = {
+          rating: resp.rating,
+        };
+        port.postMessage(msg);
+      } else {
+        prs.set(prPath, resp.rating);
+      }
+    })
+    .catch(reason => {
+      const msg: BackgroundRatingMessage = {
+        rating: prs.get(prPath),
+        error: reason
+      };
+      port.postMessage(msg);
+    });
 });
