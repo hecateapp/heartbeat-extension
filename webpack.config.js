4717bdd509d5c20d9b9ceab95c2bd44a1b9f6afc
John Barton (joho) <jrbarton@gmail.com>
2018-11-12 17:49:53 +1100

    Make package.json single point of truth for version number

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+// Have a single point of truth for version number (pull from package.json)
+const path = require('path');
+const package = require(path.join(__dirname, 'package.json'));
+const packageVersion = package.version;
+
+// Build up plugins conditionally based on environment
 const plugins = [];
 
+// Put everything in public/ into dist
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 plugins.push(new CopyWebpackPlugin([{
     from: 'public/**/*',
@@ -20,15 +27,13 @@ if (process.env.NODE_ENV !== "production") {
 
 // Dynamically construct host whitelists for the manifest
 const GenerateJsonPlugin = require('generate-json-webpack-plugin');
-const path = require('path');
-
-const manifestTemplate = require(path.join(__dirname, '.', 'manifest.json'));
+const manifestTemplate = require(path.join(__dirname, 'manifest.json'));
+manifestTemplate.version = packageVersion;
 if (process.env.NODE_ENV === "production") {
     manifestTemplate.permissions.push("https://*.hecate.co/");
 } else {
     manifestTemplate.permissions.push("http://localhost:4567/");
 }
-
 plugins.push(new GenerateJsonPlugin('manifest.json', manifestTemplate));
 
 module.exports = {
