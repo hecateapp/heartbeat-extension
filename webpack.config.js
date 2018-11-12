679f4825a6f9ebaf484de55bf077cf093a09bded
John Barton (joho) <jrbarton@gmail.com>
2018-11-12 12:42:51 +1100

    Add live reloading in dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
+const plugins = [];
+
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+plugins.push(new CopyWebpackPlugin([{
+    from: 'public/**/*',
+    flatten: true,
+}]));
+
+if (process.env.NODE_ENV !== "production") {
+    const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
+    plugins.push(new ChromeExtensionReloader({
+        port: 9090, // Which port use to create the server
+        entries: { //The entries used for the content/background scripts
+            contentScript: ['popup', 'github', 'options'], //Use the entry names, not the file name or the path
+            background: 'background'
+        }
+    }));
+}
 
 module.exports = {
     entry: {
@@ -37,10 +54,5 @@ module.exports = {
         ]
     },
 
-    plugins: [
-        new CopyWebpackPlugin([{
-            from: 'public/**/*',
-            flatten: true,
-        }])
-    ],
+    plugins: plugins,
 };
